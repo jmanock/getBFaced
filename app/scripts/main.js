db10657f6c7aea1040301567768cd073909c44d4
jmanock <jonmanock@gmail.com>
2015-07-06 11:37:37 -0400

    got links to hide/show now to get the profile to work on the hide show

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,13 +2,8 @@ $(document).ready(function(){
   $('.nav li').on('click',function(){
     $(this).addClass('active').siblings().removeClass('active');
   });
-  $('.about').click(function(){
-    $('#about').show();
-  });
-  $('.contact').click(function(){
-    $('#contact').show();
-  });
-  $('.resume').click(function(){
-    $('#resume').show();
+  $('li a').on('click', function(){
+    $('section').hide();
+    $(this.getAttribute('href')).show();
   });
 });
