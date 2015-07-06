2da12291e2be87bce25409886633846641f00067
jmanock <jonmanock@gmail.com>
2015-07-06 11:20:08 -0400

    working on hide/show sections

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,4 +2,13 @@ $(document).ready(function(){
   $('.nav li').on('click',function(){
     $(this).addClass('active').siblings().removeClass('active');
   });
+  $('.about').click(function(){
+    $('#about').show();
+  });
+  $('.contact').click(function(){
+    $('#contact').show();
+  });
+  $('.resume').click(function(){
+    $('#resume').show();
+  });
 });
