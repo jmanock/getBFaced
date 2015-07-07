131fb6e28daee1afd6c3ba74d2043de966b5a3a0
jmanock <jonmanock@gmail.com>
2015-07-07 10:44:39 -0400

    thinking about how I want to do this

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,35 @@
 $(document).ready(function(){
+  // Handels clicking nav bar links
   $('.nav li').on('click',function(){
     $(this).addClass('active').siblings().removeClass('active');
   });
+
+  // Hides/Shows the content
   $('li a').on('click', function(){
     $('section').hide();
     $(this.getAttribute('href')).show();
-    console.log('is this click showing up?? ' + this);
+    if(this.text === 'Resume'){
+      Resume(this);
+    }else if(this.text === 'About'){
+      About(this);
+    }else if(this.text =='Contact'){
+      Contact(this);
+    }
+
   });
+
+  // Thinking of ways to make the files in jQuery not HTML
+  About = function(x){
+    console.log('this is the about function' + x);
+  };
+
+  Resume = function(x){
+
+    console.log('this is the resume function ' + x);
+  };
+
+  Contact = function(x){
+    
+    console.log('this is the contact function' + x);
+  };
 });
