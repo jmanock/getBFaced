93648b6f5c0eea0d0020458e88eb5792b6d595ab
jmanock <jonmanock@gmail.com>
2016-08-10 16:09:02 -0400

    a few bugs but looking good

---
+++ assets/scripts/main.js
@@ -0,0 +1,26 @@
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(x){
+  showDivs(slideIndex += x);
+}
+
+function showDivs(x){
+  var i;
+  var n = document.getElementsByClassName('slide');
+  if(x > n.length){
+    slideIndex = 1;
+  }
+  if(x < 1){
+    slideIndex = n.length;
+  }
+  for(i=0; i < n.length; i++){
+    n[i].style.display = 'none';
+  }
+  if(x === 1){
+    console.log('This is me in a scary halloween facepait');
+  }
+  n[slideIndex - 1].style.display = 'block';
+}
+// So I do have the numbers of the photos
+// Need to add a caption or atleast a trigger to them
