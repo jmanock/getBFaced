6920d5717dd624c8cacd1024d85135adb6c11625
jmanock <jonmanock@gmail.com>
2016-08-10 16:26:34 -0400

    break time

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -17,9 +17,7 @@ function showDivs(x){
   for(i=0; i < n.length; i++){
     n[i].style.display = 'none';
   }
-  if(x === 1){
-    console.log('This is me in a scary halloween facepait');
-  }
+  
   n[slideIndex - 1].style.display = 'block';
 }
 // So I do have the numbers of the photos
